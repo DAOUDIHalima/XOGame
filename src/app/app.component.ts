2c08e429e25ff6d10bf5cb78be6674e3e15272ca
DAOUDI Halima <daoudi.halima@yahoo.com>
2024-03-03 13:29:42 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SquareComponent } from "./square/square.component";
+import { BoardComponent } from './board/board.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, BoardComponent,CommonModule]
 })
 export class AppComponent {
-  title = 'myapp';
+
 }
